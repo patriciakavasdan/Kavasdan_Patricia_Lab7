d5716bbfde6aae2de8d2039245649863184354b2
patriciakavasdan@yahoo.com <patriciakavasdan@yahoo.com>
2022-12-06 10:25:10 +0200

    Laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,26 @@
-﻿namespace Kavasdan_Patricia_Lab7;
+﻿using System;
+using Kavasdan_Patricia_Lab7.Data;
+using System.IO;
+
+namespace Kavasdan_Patricia_Lab7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+    public App()
 	{
 		InitializeComponent();
 
---
+++ ListPage.xaml.cs
@@ -0,0 +1,22 @@
+namespace Kavasdan_Patricia_Lab7;
+using Kavasdan_Patricia_Lab7.Models;
+public partial class ListPage : ContentPage
+{
+	public ListPage()
+	{
+		InitializeComponent();
+	}
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        slist.Date = DateTime.UtcNow;
+        await App.Database.SaveShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        await App.Database.DeleteShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+}
